d6e2e5a6dfd8396333e7b59a6921d4f7e2f3b942
Fra-Tsuna <68866971+Fra-Tsuna@users.noreply.github.com>
2021-01-24 11:06:57 +0100

    Update models.py

--- models.py
+++ models.py
@@ -30,3 +30,22 @@ class CriticNetwork(Model):
             state_action = tf.concat([state, action], axis=1)
             out = self.net(state_action)
             return out
+        
+class ValueNetwork():
+    def __init__(self, beta, input_dims, l1_dims=256, l2_dims=256):
+        self.input_dims=input_dims
+        self.l1_dims=l1_dims
+        self.l2_dims=l2_dims
+
+        self.model=Sequential()
+        self.model.add(Input(*self.input_dims,))
+        self.model.add(Dense(self.l1_dims))
+        self.model.add(ReLU)
+        self.model.add(Dense(self.l2_dims))
+        self.model.add(ReLU)
+        self.model.add(Dense(1))
+        self.optimizer=Adam(learning_rate=beta)
+        self.model.compile(optimizer=optimizer)
+    
+    def forward(self, state):
+        return self.model.predict(state)
