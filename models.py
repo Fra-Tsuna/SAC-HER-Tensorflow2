304ea9acbc78c05316e5eaee37d09f847ae65bd1
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-01-24 11:02:29 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,26 +1,32 @@
+#!/usr/bin/env python3
+
+
 import numpy as np
 import tensorflow as tf
-import keras
-from keras import *
-from keras.layers import *
-from tensorflow.keras.optimizers import Adam
+from tf.keras import \
+    Model, Sequential, Input, layers
+from tf.keras.optimizers import Adam
 
-class CriticNetwork():
-    def __init__(self, beta, input_dims, num_actions, l1_dims=256, l2_dims=256):
-        self.input_dims=input_dims
-        self.l1_dims=l1_dims
-        self.l2_dims=l2_dims
-        self.num_actions=num_actions
 
-        self.model=Sequential()
-        self.model.add(Input(self.input_dims[0]+num_actions,))
-        self.model.add(Dense(self.l1_dims))
-        self.model.add(ReLU)
-        self.model.add(Dense(self.l2_dims))
-        self.model.add(ReLU)
-        self.model.add(Dense(1))
-        self.optimizer=Adam(learning_rate=beta)
-        self.model.compile(optimizer=optimizer)
-    
-    def forward(self, state, action):
-        return self.model.predict(concatenate(state, action))
+# Hyperparameters
+CRITIC_DENSE_1 = 128
+CRITIC_DENSE_2 = 128
+
+
+class CriticNetwork(Model):
+
+    def __init__(self, beta, input_dim, action_dim):
+        super.__init__()
+
+        self.net = Sequential()
+        self.net.add(Input(shape=input_dim))
+        self.net.add(layers.Dense(CRITIC_DENSE_1))
+        self.net.add(layers.ReLU())
+        self.net.add(layers.Dense(CRITIC_DENSE_2))
+        self.net.add(layers.ReLU())
+        self.net.add(layers.Dense(1))
+
+        def forward(self, state, action):
+            state_action = tf.concat([state, action], axis=1)
+            out = self.net(state_action)
+            return out
