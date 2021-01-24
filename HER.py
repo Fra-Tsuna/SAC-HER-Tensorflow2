b7d6761a69c2c148dd684ec92f8efa130fdfed19
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-01-24 09:45:32 +0100

    Update HER.py

--- HER.py
+++ HER.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
+
 import numpy as np
 from collections import namedtuple, deque
 
+
 """ 
 Structure of an item in the Hindsight Experience Replay 
-N.B: || denotes concatenation
+N.B: _ denotes concatenation
 """
 Experience = namedtuple("Experience", field_names = \
-    ['state||goal', 'action', 'reward', 'new_state||goal', 'done'])
+    ['state_goal', 'action', 'reward', 'new_state_goal', 'done'])
 
 
 class HER_Buffer:
@@ -28,6 +30,25 @@ class HER_Buffer:
         exp: experience to store = [state||goal, action, reward, new_state||goal, done]
         """
         self.buffer.append(exp)
+
+    def store_experience(self, experience, reward, goal):
+        """
+        Store an experience in the HER buffer
+
+        Parameters
+        ----------
+        experience: experience to store
+        reward: the reward computed in the agent's env
+        goal: the goal to concatenate to the states
+        """
+        state = experience.state_goal
+        action = experience.action
+        new_state = experience.new_state_goal
+        done = experience.done
+        state_goal = np.concatenate([state['observation'], goal])
+        new_state_goal = np.concatenate([new_state['observation'], goal])
+        self.buffer.append(
+            Experience(state_goal, action, reward, new_state_goal, done))
 
     def sample(self, minibatch=1):
         """
