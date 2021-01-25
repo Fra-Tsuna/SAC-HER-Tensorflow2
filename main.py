6ce9d8a36a65e7ba876ea95516ed4d70fca48d92
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-01-25 10:33:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import time
 
 # Personal libraries
 from HER import HER_Buffer
+from HER_SAC_agent import HER_SAC_Agent
 
 
 # ___________________________________________________ Parameters ___________________________________________________ #
@@ -25,7 +26,8 @@ from HER import HER_Buffer
 ENV_NAME = "FetchPush-v1"
 
 # learning parameters
-RANDOM_BATCH = 1000
+TRAINING_BATCH = 50000
+RANDOM_EPISODES = 1000
 HER_CAPACITY = 1000
 
 # _____________________________________________________ Main _____________________________________________________ #
@@ -36,15 +38,18 @@ if __name__ == '__main__':
     # Environment initialization
     env = gym.make(ENV_NAME)
     obs_space = env.observation_space.shape
-    n_actions = env.action_space.shape[0]
+    actions_dim = env.action_space.shape[0]
 
     # Agent initialization
     her_buff = HER_Buffer(HER_CAPACITY)
     agent = HER_SAC_Agent(env, her_buff)
 
+    # Training 
+            
+
     # Random playing (useful for testing)
-    success = agent.random_play(RANDOM_BATCH)
-    if success:
-        print("Goal achieved! Good job!")
-    else:
-        print("Bad play...")
+    #success = agent.random_play(RANDOM_BATCH)
+    #if success:
+    #    print("Goal achieved! Good job!")
+    #else:
+    #    print("Bad play...")
