f5820ef92b98ea8728885d7e6dc7cf2bb145d8e4
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-01-29 15:01:20 +0100

    Update HER_SAC_agent.py

--- HER_SAC_agent.py
+++ HER_SAC_agent.py
@@ -10,8 +10,8 @@ from models import ActorNetwork, CriticNetwork, ValueNetwork
 
 # Learning parameters
 REWARD_SCALE = 10
-LEARNING_RATE = 1e-4
-GAMMA = 0.99
+LEARNING_RATE = 0.0004
+GAMMA = 0.98
 TAU = 0.005
 
 
@@ -39,7 +39,6 @@ class HER_SAC_Agent:
         self.critic_2 = CriticNetwork(self.critic_state_shape)
         self.value = ValueNetwork(self.normal_state_shape)
         self.target_value = ValueNetwork(self.normal_state_shape)
-        self._targetbuild()
 
         # optimizers
         if optimizer == 'Adam':
@@ -54,17 +53,10 @@ class HER_SAC_Agent:
             self.value_optimizer = None
             print("Error: wrong or not supported optimizer")
 
-    def _targetbuild(self):
-        """
-        Required to build target value network
-        """
-        arr = np.array(np.zeros(self.normal_state_shape), ndmin=2)
-        dummy_build = self.target_value(arr)
-
     def getBuffer(self):
         return self.her_buffer
 
-    def play_episode(self, criterion="random", epsilon=0):
+    def play_episode(self, criterion="random", epsilon=0):                          
         """
         Play an episode choosing actions according to the selected criterion
         
@@ -75,7 +67,7 @@ class HER_SAC_Agent:
 
         Returns
         -------
-        experiences: all experiences taken by the agent in the episode 
+        experiences: all experiences taken by the agent in the episode
         """
         state = self.env.reset()
         experiences = []
@@ -100,7 +92,7 @@ class HER_SAC_Agent:
             new_state, reward, done, _ = self.env.step(action)
             experiences.append(Experience(state, action, reward, new_state, done))
             state = new_state
-            print("\tStep: ", step, "Reward = ", reward)
+            print("\tStep: ", step, "Reward = ", reward)        
         return experiences
 
     def optimization(self, minibatch):
@@ -133,20 +125,22 @@ class HER_SAC_Agent:
             q2 = self.critic_2(states, actions)
             q = tf.minimum(q1, q2)
             v = self.value(states)
-            value_loss = 0.5 * tf.reduce_mean((v - (q-log_probs))**2)      
+            value_loss = 0.5 * tf.reduce_mean((v - (q-log_probs))**2)
         variables = self.value.trainable_variables
         value_grads = value_tape.gradient(value_loss, variables)
         self.value_optimizer.apply_gradients(zip(value_grads, variables))
 
         # 3° step: optimize critic networks
         with tf.GradientTape() as critic1_tape:
-            v_tgt = self.value(new_states)
-            q_tgt = tf.stop_gradient(REWARD_SCALE*rewards + GAMMA*v_tgt)
+            v_tgt = self.target_value(new_states)
+            q_tgt = tf.stop_gradient([REWARD_SCALE*r for r in rewards] + 
+                                     GAMMA*([not d for d in dones]*v_tgt))
             q1 = self.critic_1(states, exp_actions)
             critic1_loss = 0.5 * tf.reduce_mean((q1 - q_tgt)**2)
         with tf.GradientTape() as critic2_tape:
-            v_tgt = self.value(new_states)
-            q_tgt = tf.stop_gradient(REWARD_SCALE*rewards + GAMMA*v_tgt)
+            v_tgt = self.target_value(new_states)
+            q_tgt = tf.stop_gradient([REWARD_SCALE*r for r in rewards] + 
+                                     GAMMA*([not d for d in dones]*v_tgt))
             q2 = self.critic_2(states, exp_actions)
             critic2_loss = 0.5 * tf.reduce_mean((q2 - q_tgt)**2)
         variables_c1 = self.critic_1.trainable_variables
@@ -166,16 +160,16 @@ class HER_SAC_Agent:
         variables = self.actor.trainable_variables
         actor_grads = actor_tape.gradient(actor_loss, variables)
         self.actor_optimizer.apply_gradients(zip(actor_grads, variables))
+        
+        return value_loss, critic1_loss, critic2_loss, actor_loss
 
-        #5° step: update target network
+    def update_target(self):
         target_value_variables = self.target_value.trainable_variables
         value_variables = self.value.trainable_variables
         for var in range(len(value_variables)):
             value_variables[var] = \
                 TAU*value_variables[var] + (1-TAU)*target_value_variables[var]
-        self.target_value.net.set_weights(value_variables)
-
-        return value_loss, critic1_loss, critic2_loss, actor_loss
+        self.target_value.set_weights(value_variables)
 
     def random_play(self, batch_size):
         """
