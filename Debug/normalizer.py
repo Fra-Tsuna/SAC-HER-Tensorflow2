b82d6eaec9a528f46a6f35b19d91f0a7dd5fbe52
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-02-03 14:59:19 +0100

    Update normalizer.py

--- Debug/normalizer.py
+++ Debug/normalizer.py
@@ -1,36 +1,67 @@
 import numpy as np
 
+DEBUG_NORMALIZER = False
+NORMALIZATION = "Gaussian"
+
 class Normalizer:
-    def __init__(self, size, eps=1e-2, clip_range=np.inf):
+    def __init__(self, size, eps=1e-2, clip_range=np.inf, normalization=NORMALIZATION):
         self.size = size
         self.eps = eps
         self.clip_range = clip_range
+        self.normalization = normalization
 
-        # some local information
-        self.local_sum = np.zeros(self.size, np.float32)
-        self.local_sumsq = np.zeros(self.size, np.float32)
-        self.local_count = np.zeros(1, np.float32)
+        if self.normalization == "Gaussian":
+            self.local_sum = np.zeros(self.size, np.float32)
+            self.local_sumsq = np.zeros(self.size, np.float32)
+            self.local_count = np.zeros(1, np.float32)
+            self.mean = np.zeros(self.size, np.float32)
+            self.std = np.ones(self.size, np.float32)
+        elif self.normalization == "MinMax":
+            self.min = np.zeros(self.size, np.float32)
+            self.max = np.ones(self.size, np.float32)
+        else:
+            raise TypeError("Wrong normalization type")
 
-        # get the total sum sumsq and sum count
-        self.total_sum = np.zeros(self.size, np.float32)
-        self.total_sumsq = np.zeros(self.size, np.float32)
-        self.total_count = np.ones(1, np.float32)
-
-        # get the mean and std
-        self.mean = np.zeros(self.size, np.float32)
-        self.std = np.ones(self.size, np.float32)
-    
-    def update(self, v):
-        v = v.reshape(-1, self.size)
-        self.local_sum += v.sum(axis=0)
-        self.local_sumsq += (np.square(v)).sum(axis=0)
-        self.local_count[0] += v.shape[0]
-
-    def recompute_stats(self):
-        self.mean = self.local_sum / self.local_count
-        self.std = np.sqrt(np.maximum(np.square(self.eps), (self.local_sumsq / self.local_count) - np.square(self.local_sum / self.local_count)))
+    def update(self, buffer):
+        buffer = np.array(buffer)
+        if self.normalization == "Gaussian":
+            self.local_sum += buffer.sum(axis=0)
+            self.local_sumsq += (np.square(buffer)).sum(axis=0)
+            self.local_count[0] += buffer.shape[0]
+            self.mean = self.local_sum / self.local_count
+            self.std = np.sqrt(np.maximum(np.square(self.eps), (self.local_sumsq / self.local_count) - np.square(self.local_sum / self.local_count)))
+        elif self.normalization == "MinMax":
+            self.min = np.minimum(self.min, buffer.min(axis=0))
+            self.max = np.maximum(self.max, buffer.max(axis=0))
 
     def normalize(self, v, clip_range=None):
         if clip_range is None:
             clip_range = self.clip_range
-        return np.clip((v - self.mean) / (self.std), -clip_range, clip_range)
+        if self.normalization == "Gaussian":
+            v_norm = (v - self.mean) / self.std
+        elif self.normalization == "MinMax":
+            v_norm = (v - self.min) / (self.max - self.min)
+        if DEBUG_NORMALIZER:
+            print("++++++++++++++++ DEBUG - NORMALIZER [NORMALIZER.NORMALIZE] ++++++++++++++++\n")
+            if self.normalization == "Gaussian":
+                print("----------------------------mean----------------------------")
+                print(self.mean)
+                print("----------------------------std-dev----------------------------")
+                print(self.std)
+                print("----------------------------local_sum----------------------------")
+                print(self.local_sum)
+                print("----------------------------local_count----------------------------")
+                print(self.local_count)
+                print("----------------------------local_sumsq----------------------------")
+                print(self.local_sumsq)
+            elif self.normalization == "MinMax":
+                print("----------------------------mean----------------------------")
+                print(self.min)
+                print("----------------------------std-dev----------------------------")
+                print(self.max)
+            print("----------------------------vector----------------------------")
+            print(v)
+            print("----------------------------v_norm----------------------------")
+            print(v_norm)
+            a = input("\n\nPress Enter to continue...") 
+        return np.clip(v_norm, -clip_range, clip_range)
