4c3095ab68458a0c65d423e659d0480427f40793
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-02-08 14:39:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,43 +3,44 @@
 # ___________________________________________________ Libraries ___________________________________________________ #
 
 
-# Learning libraries
-import tensorflow as tf
+# Learning
 import gym
+import tensorflow as tf
 from tensorboardX import SummaryWriter
 
-# Math libraries
-import numpy as np
+# Math 
 import random
+import numpy as np
 
-# Personal libraries
+# Custom libraries
 from HER import HER_Buffer, Experience
 from HER_SAC_agent import HER_SAC_Agent
 
 # ___________________________________________________ Parameters ___________________________________________________ #
 
 
-# environment parameters
+# Environment 
 ENV_NAME = "FetchReach-v1"
 LOG_DIR = "/home/gianfranco/Desktop/FetchLog"
 
-# training parameters
+# Training 
 TRAINING_EPOCHES = 200
 BATCH_SIZE = 50
 OPTIMIZATION_STEPS = 40
-POLICY_STEPS = 8
+POLICY_STEPS = 2
 RANDOM_EPISODES = 1000
 EVAL_EPISODES = 10
 
-# HER parameters
+# HER 
 HER_CAPACITY = 1000000
 REPLAY_START_SIZE = 1000
-MINIBATCH_SAMPLE_SIZE = 128
+MINIBATCH_SAMPLE_SIZE = 256
 STRATEGY = "future"
 FUTURE_K = 4
 
-# learning parameters
-EPSILON_START = 0.7
+# Learning parameters
+EPSILON_START = 0.5
+EPSILON_NEXT = 0.1
 
 # _____________________________________________________ Main _____________________________________________________ #
 
@@ -63,7 +64,7 @@ if __name__ == '__main__':
 
     # Training 
     for epoch in range(TRAINING_EPOCHES):
-        print("\n\n************ TRAINING EPOCH ", epoch, "************\n")
+        print("\n\n___________ TRAINING EPOCH ", epoch, "___________\n")
         reward_vect = []
 
         ## play batch of cycles
@@ -73,16 +74,14 @@ if __name__ == '__main__':
 
             ### play episodes
             for policy_step in range(POLICY_STEPS):
-                #print("\t____Policy episode ", policy_step, "____")
                 experiences = agent.play_episode(criterion="SAC", epsilon=epsilon)
-                goal = experiences[-1].state['desired_goal']
+                goal = experiences[0].state['desired_goal']
                 iterations += len(experiences)
                 for t in range(len(experiences)):
                     reward_vect.append(experiences[t].reward)
                     achieved_goal = experiences[t].new_state['achieved_goal']
-                    true_reward = agent.env.compute_reward(achieved_goal, goal, None)
                     hindsight_exp = agent.getBuffer().store_experience(experiences[t], 
-                                                                    true_reward, goal)
+                                                                    experiences[t].reward, goal)
                     hindsight_experiences.append(hindsight_exp)
                     if STRATEGY == "final":
                         desired_goal = experiences[-1].new_state['achieved_goal']
@@ -99,21 +98,22 @@ if __name__ == '__main__':
                                 agent.env.compute_reward(achieved_goal, desired_goal, None)
                             hindsight_exp = agent.getBuffer().store_experience(experiences[t],
                                                                     her_reward, desired_goal)
-                            hindsight_experiences.append(hindsight_exp)    
+                            hindsight_experiences.append(hindsight_exp)
+                    else:
+                        raise TypeError("Wrong strategy for goal sampling. \
+                                        [available 'final' or 'future']")   
 
                 #### print results
-                if iterations > 5000:
-                    m_reward_5000 = np.mean(reward_vect[-5000:])
-                    writer.add_scalar("mean_reward_5000", m_reward_5000, iterations)
+                mean_reward = np.mean(reward_vect)
+                writer.add_scalar("mean_reward", mean_reward, iterations)
 
             ### normalization
             agent.update_normalizer(batch=hindsight_experiences, hindsight=True)
 
             ### optimization
             if len(agent.getBuffer()) >= REPLAY_START_SIZE:
-                epsilon = 0.2
+                epsilon = EPSILON_NEXT
                 for opt_step in range(OPTIMIZATION_STEPS):
-                    #print("__Optimization step ", opt_step, "__")
                     minibatch = agent.getBuffer().sample(MINIBATCH_SAMPLE_SIZE)
                     v_loss, c1_loss, c2_loss, act_loss = \
                         agent.optimization(minibatch)
@@ -129,4 +129,4 @@ if __name__ == '__main__':
             success_rates.append(success_rate)
         success_rate = sum(success_rates) / len(success_rates)
         print("Success_rate = ", success_rate)
-        writer.add_scalar("success rate per epoch", success_rate, epoch)
+        writer.add_scalar("mean success rate per epoch", success_rate, epoch)
