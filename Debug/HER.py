c5884810bcbb2b1a0430f9ef28bc12d521056b52
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-02-08 14:43:51 +0100

    Update HER.py

--- Debug/HER.py
+++ Debug/HER.py
@@ -1,22 +1,18 @@
-
 #!/usr/bin/env python3
 
-
-import numpy as np
-from collections import namedtuple, deque
-import tensorflow as tf
 import time
+import numpy as np
+import tensorflow as tf
+from collections import namedtuple, deque
 
 
 """ 
 Structure of a single Experience
-N.B: state can be anything (observation, dictionary, ecc.), but
-     the exp. stored in the HER buffer should have as state the observation
-     concatenated with the goal
+N.B: the exp. stored in the HER buffer should have as state (new state) 
+     the observation (new observation) concatenated with the goal
 """
 Experience = namedtuple("Experience", field_names = \
     ['state', 'action', 'reward', 'new_state', 'done'])
-
 
 # debug parameters
 DEBUG_EXP = False
@@ -33,78 +29,56 @@ class HER_Buffer:
         return len(self.buffer)
 
     def append(self, exp):
-        """
-        Insert new element to the buffer (if full, old memory is dropped)
-        Parameters
-        ----------
-        exp: experience to store = [state_goal, action, reward, newState_goal, done]
-            N.B: _ denotes concatenation
-        """
         self.buffer.append(exp)
 
     def store_experience(self, experience, reward, goal):
         """
         Store an experience in the HER buffer
+
         Parameters
         ----------
         experience: experience to store
         reward: the reward computed in the agent's env
         goal: the goal to concatenate to the states
+
+        Return
+        -------
+        hindsight_exp: experience in the hindsight representation:
+            (state||goal, action, reward, new_state||goal, done).
+            N.B: || denotes concatenation
         """
-        hindsight_exp = self._hindsight_representation(experience, reward, goal)
-        if DEBUG_EXP:
-            print("\n\n\t++++++++++++++++ DEBUG - STORE EXPERIENCES [HER.STORE_EXPERIENCE] +++++++++++++++++\n")
-            time.sleep(1)
-            print("\t----------------------------state||goal----------------------------")
-            print("\t", state_goal)
-            print("\t----------------------------action----------------------------")
-            print("\t", action)
-            print("\t----------------------------her reward----------------------------")
-            print("\t", reward)
-            print("\t----------------------------new_state||goal----------------------------")
-            print("\t", newState_goal)
-            print("\t----------------------------done----------------------------")
-            print("\t", done)
-            a = input("\n\n\tPress Enter to continue...")
-        """
-        print("Element stored in (or sampled by) the HER buffer: \n")
-        print("---------------------------")
-        print("state||goal\n", state_goal)
-        print("action\n", action)
-        print("reward\n", reward)
-        print("new_state||goal\n", newState_goal)
-        print("done\n", done)
-        print("---------------------------")
-        """
+        hindsight_exp = \
+            self._hindsight_representation(experience, reward, goal)
         self.buffer.append(hindsight_exp)
         if DEBUG_BUFFER:
-            print("\n\n\t++++++++++++++++ DEBUG - BUFFER [HER.STORE_EXPERIENCE] +++++++++++++++++\n")
-            time.sleep(1)
-            print("\t----------------------------buffer----------------------------")
-            print("\t", self.buffer)
-            a = input("\n\n\tPress Enter to continue...")
+            print("\n\n\t++++++++++++++++ DEBUG - BUFFER [HER.STORE_EXP] +++++++++++++++++\n")
+            print("\t----------------------------len----------------------------")
+            print("\t", len(self.buffer))
+            a = input("\tPress Enter to continue...")
         return hindsight_exp
 
-    def sample(self, minibatch=1):
+    def sample(self, minibatch_size=1):
         """
         Sample items from the buffer
+
         Parameters
         ----------
         minibatch: number of items to sample from the buffer
-        Returns
+
+        Return
         -------
-        items: items sampled from the buffer
+        items: hindsight experiences sampled from the buffer
         """
-        locations = np.random.choice(len(self.buffer), minibatch, replace=False)            
-        if minibatch == 1:
+        locations = np.random.choice(len(self.buffer), 
+                        minibatch_size, replace=False)
+        items = []
+        if minibatch_size == 1:
             items = self.buffer[locations[0]]
         else:
-            items = []
             for index in locations:
                 items.append(self.buffer[index])
         if DEBUG_SAMPLE:
             print("\n\n\t++++++++++++++++ DEBUG - SAMPLE [HER.SAMPLE] +++++++++++++++++\n")
-            time.sleep(1)
             print("\t----------------------------indexes----------------------------")
             print("\t", locations)
             a = input("\n\n\tPress Enter to continue...")
@@ -112,12 +86,12 @@ class HER_Buffer:
 
     def _hindsight_representation(self, experience, reward, goal):
         """
-        Convert the experience in input into the her canonical representation
+        Convert the passed experience to the HER canonical representation
         
         Parameters
         ----------
         experience: experience to convert
-        goal: the goal obtained with the opportune strategy
+        goal: the goal obtained with any sampling strategy
         
         Return
         ------
@@ -129,4 +103,17 @@ class HER_Buffer:
         done = experience.done
         state_goal = np.concatenate([state['observation'], goal])
         newState_goal = np.concatenate([newState['observation'], goal])
+        if DEBUG_EXP:
+            print("\n\n\t++++++++++++++++ DEBUG - STORE EXPERIENCES [HER.HINDSIGHT_REPRESENTATION] +++++++++++++++++\n")
+            print("\t----------------------------state||goal----------------------------")
+            print("\t", state_goal)
+            print("\t----------------------------action----------------------------")
+            print("\t", action)
+            print("\t----------------------------her reward----------------------------")
+            print("\t", reward)
+            print("\t----------------------------new_state||goal----------------------------")
+            print("\t", newState_goal)
+            print("\t----------------------------done----------------------------")
+            print("\t", done)
+            a = input("\n\n\tPress Enter to continue...")
         return Experience(state_goal, action, reward, newState_goal, done)
