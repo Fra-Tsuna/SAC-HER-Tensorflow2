c2e00a3debc68e42d59ddde7f3ce648ba663b8e9
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-02-08 14:52:32 +0100

    Update normalizer.py

--- Debug/normalizer.py
+++ Debug/normalizer.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import time
 import tensorflow as tf
+
 
 DEBUG_NORMALIZE = False
 DEBUG_UPDATE = False
 NORMALIZATION = "Gaussian"
 
+
 class Normalizer:
-    def __init__(self, size, eps=1e-2, clip_range=np.inf, normalization=NORMALIZATION):
+    def __init__(self, size, eps=1e-2, clip_range=np.inf, 
+                 normalization=NORMALIZATION):
         self.size = size
         self.eps = eps
         self.clip_range = clip_range
@@ -27,18 +31,18 @@ class Normalizer:
             raise TypeError("Wrong normalization type")
 
     def update(self, buffer):
-        #buffer = np.array(buffer)
-        #elem = [a[3] for a in buffer if buffer.shape[1]>3]
-        #print(elem)
-        if self.normalization == "Gaussian":
-            self.local_sum += buffer.sum(axis=0)
-            self.local_sumsq += (np.square(buffer)).sum(axis=0)
-            self.local_count[0] += buffer.shape[0]
-            self.mean = self.local_sum / self.local_count
-            self.std = np.sqrt(np.maximum(np.square(self.eps), (self.local_sumsq / self.local_count) - np.square(self.local_sum / self.local_count)))
-        elif self.normalization == "MinMax":
-            self.min = np.minimum(self.min, buffer.min(axis=0))
-            self.max = np.maximum(self.max, buffer.max(axis=0))
+        with self.lock:
+            if self.normalization == "Gaussian":
+                self.local_sum += buffer.sum(axis=0)
+                self.local_sumsq += (np.square(buffer)).sum(axis=0)
+                self.local_count[0] += buffer.shape[0]
+                self.mean = self.local_sum / self.local_count
+                self.std = np.sqrt(np.maximum(np.square(self.eps), 
+                                   (self.local_sumsq / self.local_count) - 
+                                   np.square(self.local_sum / self.local_count)))
+            elif self.normalization == "MinMax":
+                self.min = np.minimum(self.min, buffer.min(axis=0))
+                self.max = np.maximum(self.max, buffer.max(axis=0))
         if DEBUG_UPDATE:
             print("++++++++++++++++ DEBUG - UPDATE NORMALIZER [NORMALIZER.UPDATE] ++++++++++++++++\n")
             if self.normalization == "Gaussian":
@@ -58,14 +62,14 @@ class Normalizer:
                 print("----------------------------std-dev----------------------------")
                 print(self.max)
             a = input("\n\nPress Enter to continue...")
-
-    def normalize(self, v, clip_range=None):
+        
+    def normalize(self, vector, clip_range=None):
         if clip_range is None:
             clip_range = self.clip_range
         if self.normalization == "Gaussian":
-            v_norm = (v - self.mean) / self.std
+            v_norm = (vector - self.mean) / self.std
         elif self.normalization == "MinMax":
-            v_norm = (v - self.min) / (self.max - self.min)
+            v_norm = (vector - self.min) / (self.max - self.min)
         if DEBUG_NORMALIZE:
             print("++++++++++++++++ DEBUG - NORMALIZE [NORMALIZER.NORMALIZE] ++++++++++++++++\n")
             if self.normalization == "Gaussian":
@@ -88,5 +92,5 @@ class Normalizer:
             #print(v)
             #print("----------------------------v_norm----------------------------")
             #print(v_norm)
-            #a = input("\n\nPress Enter to continue...") 
+            a = input("\n\nPress Enter to continue...")
         return np.clip(v_norm, -clip_range, clip_range)
