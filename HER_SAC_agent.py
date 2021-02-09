fbaf260b877abd91db8a0e909bc2b3a6f98f27b9
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-02-09 11:21:09 +0100

    Update HER_SAC_agent.py

--- HER_SAC_agent.py
+++ HER_SAC_agent.py
@@ -12,8 +12,10 @@ from models import ActorNetwork, CriticNetwork, ValueNetwork
 
 
 # Learning parameters
-REWARD_SCALE = 20
-LEARNING_RATE = 3e-4
+MINIBATCH_SAMPLE_SIZE = 256
+LEARNING_RATE = 0.001
+LR_TEMPERATURE = 3e-4
+MIN_TEMPERATURE = 0.05
 GAMMA = 0.98
 TAU = 0.005
 NORM_CLIP_RANGE = 5
@@ -22,7 +24,7 @@ CLIP_MAX = 200
 
 class HER_SAC_Agent:
 
-    def __init__(self, env, her_buffer, optimizer='Adam'):
+    def __init__(self, env, her_buffer, temperature="auto", optimizer='Adam'):
 
         # env
         self.env = env
@@ -46,6 +48,19 @@ class HER_SAC_Agent:
         self.value = ValueNetwork(self.normal_state_shape)
         self.target_value = ValueNetwork(self.normal_state_shape)
 
+        # temperature parameters
+        if temperature == "auto":
+            self.log_temperature = tf.Variable(tf.math.log(1.0), dtype=tf.float32)
+            self.target_entropy = -tf.constant(self.action_size, dtype=tf.float32)
+        elif isinstance(temperature, float):
+            if 0 < temperature <= 1:
+                self.log_temperature = tf.math.log(temperature)
+            else:
+                raise ValueError("Temperature parameter must be in range ]0,1]")
+        else:
+            raise TypeError("Wrong temperature coefficient. \
+                            [available 'auto' or float in ]0,1] range]")
+
         # normalizers
         self.state_norm = Normalizer(size=self.obs_size, clip_range=NORM_CLIP_RANGE)
         self.goal_norm = Normalizer(size=self.goal_size, clip_range=NORM_CLIP_RANGE)
@@ -62,11 +77,15 @@ class HER_SAC_Agent:
             self.critic1_optimizer = Adam(LEARNING_RATE)
             self.critic2_optimizer = Adam(LEARNING_RATE)
             self.value_optimizer = Adam(LEARNING_RATE)
+            if temperature == "auto":
+                self.temperature_optimizer = Adam(LR_TEMPERATURE)
         elif optimizer == 'Rectified_Adam':
             self.actor_optimizer = RectifiedAdam(LEARNING_RATE)
             self.critic1_optimizer = RectifiedAdam(LEARNING_RATE)
             self.critic2_optimizer = RectifiedAdam(LEARNING_RATE)
             self.value_optimizer = RectifiedAdam(LEARNING_RATE)
+            if temperature == "auto":
+                self.temperature_optimizer = RectifiedAdam(LR_TEMPERATURE)
         else:
             raise TypeError("Wrong or not supported optimizer. \
                             [availiable 'Adam' or 'Rectified_Adam']")
@@ -76,6 +95,14 @@ class HER_SAC_Agent:
         return the replay buffer of the agent
         """
         return self.her_buffer
+
+    def getTemperature(self):
+        """
+        return the entropy temperature coefficient
+        """
+        temperature_t = tf.exp(self.log_temperature)
+        temperature = temperature_t.numpy()
+        return temperature
 
     def play_episode(self, criterion="random", epsilon=0):                          
         """
@@ -94,7 +121,7 @@ class HER_SAC_Agent:
         experiences = []
         done = False
         t = 0
-        while t < self.max_timesteps:
+        while t < self.max_timesteps and not done:
             t += 1
             self.env.render()
             if criterion == "random":
@@ -112,13 +139,14 @@ class HER_SAC_Agent:
                     action = action.numpy()[0]
             else:
                 raise TypeError("Wrong criterion for choosing the action. \
-                                [available 'random' or 'SAC']")
-            new_state, reward, done, _ = self.env.step(action)
+                                [available 'random' or 'SAC']")                
+            new_state, reward, done, info = self.env.step(action)
+            done = done or bool(info['is_success'])
             experiences.append(Experience(state, action, reward, new_state, done))
             state = new_state
         return experiences
 
-    def optimization(self, minibatch):
+    def optimization(self, minibatch=None):
         """
         Update networks in order to learn the correct policy
 
@@ -132,11 +160,14 @@ class HER_SAC_Agent:
         """
         # 1° step: unzip minibatch sampled from HER
         exp_actions, rewards, dones = [], [], []
+        if minibatch is None:
+            minibatch = self.her_buffer.sample(MINIBATCH_SAMPLE_SIZE)
         for exp in minibatch:
             exp_actions.append(exp.action)
             rewards.append(exp.reward)
             dones.append(exp.done)
         states, new_states = self.preprocess_inputs(minibatch)
+        del minibatch
 
         # 2° step: optimize value network
         actions, log_probs = self.actor(states, noisy=False)
@@ -151,13 +182,14 @@ class HER_SAC_Agent:
             zip(value_grads, self.value.trainable_variables))
 
         # 3° step: optimize critic networks
-        v_tgt = tf.reshape(self.target_value(new_states), -1)
-        q_tgt = [REWARD_SCALE*r for r in rewards] + GAMMA*([not d for d in dones]*v_tgt)
+        v_tgt = self.target_value(new_states)
+        q_tgt = (1/tf.exp(self.log_temperature))*rewards + GAMMA*([not d for d in dones]*tf.reshape(v_tgt, -1))
+        q_tgt = tf.reshape(q_tgt, (len(rewards), 1))
         with tf.GradientTape() as critic1_tape:
-            q1 = tf.reshape(self.critic_1(states, exp_actions), -1)
+            q1 = self.critic_1(states, exp_actions)
             critic1_loss = 0.5 * tf.reduce_mean(tf.square(q1 - q_tgt))
         with tf.GradientTape() as critic2_tape:
-            q2 = tf.reshape(self.critic_2(states, exp_actions), -1)
+            q2 = self.critic_2(states, exp_actions)
             critic2_loss = 0.5 * tf.reduce_mean(tf.square(q2 - q_tgt))
         critic1_grads = critic1_tape.gradient(critic1_loss, self.critic_1.trainable_variables)
         critic2_grads = critic2_tape.gradient(critic2_loss, self.critic_2.trainable_variables)
@@ -176,8 +208,22 @@ class HER_SAC_Agent:
         actor_grads = actor_tape.gradient(actor_loss, self.actor.trainable_variables)
         self.actor_optimizer.apply_gradients(
             zip(actor_grads, self.actor.trainable_variables))
-        
-        return value_loss, critic1_loss, critic2_loss, actor_loss
+
+        # 5° step: optimize temperature parameter
+        if not isinstance(self.log_temperature, float):
+            actions, log_probs = self.actor(states, noisy=False)
+            with tf.GradientTape() as temperature_tape: 
+                temperature_loss = \
+                    tf.reduce_mean(-tf.exp(self.log_temperature)*
+                                  (log_probs + self.target_entropy))
+            temperature_grads = \
+                temperature_tape.gradient(temperature_loss, [self.log_temperature])
+            self.temperature_optimizer.apply_gradients(
+                zip(temperature_grads, [self.log_temperature]))
+            self.log_temperature.assign(tf.maximum(self.log_temperature, 
+                tf.Variable(tf.math.log(MIN_TEMPERATURE), dtype=tf.float32)))
+
+        return value_loss, critic1_loss, critic2_loss, actor_loss, temperature_loss
 
     def soft_update(self, tau=TAU):
         """
@@ -207,7 +253,7 @@ class HER_SAC_Agent:
             g = [exp.state[-self.goal_size:] for exp in batch] 
         self.state_norm.update(np.clip(obs, -CLIP_MAX, CLIP_MAX))
         self.goal_norm.update(np.clip(g, -CLIP_MAX, CLIP_MAX))
-        
+
     def preprocess_inputs(self, her_batch):
         """
         Normalize states, goals and re-convert into HER representation
